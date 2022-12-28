bf44c8ac62e70282f90629b38811841275ce864f
nhuthq <>
2022-12-28 16:00:13 +0700

    add detaisl infor

--- components/ItemCardContainer.js
+++ components/ItemCardContainer.js
@@ -20,13 +20,13 @@ const ItemCardContainer = ({ imageSource, title, location, data }) => {
 
       {title ? (
         <>
-          <Text className="text-[18px] text-[#0B646B] font-bold">
+          <Text className="text-[18px] text-[#428288] font-bold">
             {title?.length > 14 ? `${title?.slice(0, 14)}..` : title}
           </Text>
 
           <View className="flex-row space-x-1 items-center">
-            <FontAwesome name="map-marker" size={20} color="8597A2" />
-            <Text className="text-[12px] text-[#0B646B] font-bold">
+            <FontAwesome name="map-marker" size={20} color="#8C9EA6" />
+            <Text className="text-[12px] text-[#428288] font-bold">
               {location?.length > 18 ? `${location?.slice(0, 18)}..` : location}
             </Text>
           </View>
--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -3,7 +3,7 @@ import React, { useLayoutEffect } from "react";
 import { ScrollView, TouchableOpacity } from "react-native";
 import { Image, Text, View } from "react-native-animatable";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { FontAwesome } from "@expo/vector-icons";
+import { FontAwesome, FontAwesome5, Entypo } from "@expo/vector-icons";
 
 const DetailsScreen = ({ route }) => {
   const navigation = useNavigation();
@@ -15,25 +15,113 @@ const DetailsScreen = ({ route }) => {
   return (
     <SafeAreaView className="flex-1 bg-white relative">
       <ScrollView className="flex-1 px-4 py-6">
-        <View className="flex-relative bg-white shadow-lg">
-          <Image
-            className="w-full h-72 object-cover rounded-2xl"
-            source={{
-              uri: data?.photo?.images?.large?.url
-                ? data?.photo?.images?.large?.url
-                : "https://us.123rf.com/450wm/benidict83/benidict832008/benidict83200800014/benidict83200800014.jpg?ver=6",
-            }}
-          />
-        </View>
+        <View>
+          <View className="flex-relative bg-white shadow-lg">
+            <Image
+              className="w-full h-72 object-cover rounded-2xl"
+              source={{
+                uri: data?.photo?.images?.large?.url
+                  ? data?.photo?.images?.large?.url
+                  : "https://us.123rf.com/450wm/benidict83/benidict832008/benidict83200800014/benidict83200800014.jpg?ver=6",
+              }}
+            />
+          </View>
 
-        <View className="absolute flex-row inset-x-0 top-5 justify-between px-6">
-          <TouchableOpacity className="w-10 h-10 rounded-md items-center justify-center bg-white">
-            <FontAwesome name="chevron-left" size={24} color="#06B2BE" />
-          </TouchableOpacity>
-          <TouchableOpacity className="w-10 h-10 rounded-md items-center justify-center bg-[#06B2BE]">
-            <FontAwesome name="heartbeat" size={24} color="white" />
-          </TouchableOpacity>
+          <View className="absolute flex-row inset-x-0 top-5 justify-between px-6">
+            <TouchableOpacity
+              className="w-10 h-10 rounded-md items-center justify-center bg-white"
+              onPress={() => {
+                navigation.goBack();
+              }}
+            >
+              <FontAwesome5 name="chevron-left" size={24} color="#06B2BE" />
+            </TouchableOpacity>
+            <TouchableOpacity className="w-10 h-10 rounded-md items-center justify-center bg-[#06B2BE]">
+              <FontAwesome name="heartbeat" size={24} color="white" />
+            </TouchableOpacity>
+          </View>
+
+          <View className="absolute flex-row inset-x-0 space-x-2 bottom-5 justify-between px-6">
+            <View className="flex-row items-center space-x-2">
+              <Text className="text-[12px] font-bold text-gray-100">
+                {data?.price_level}
+              </Text>
+              <Text className="text-[18px] font-bold text-gray-100">
+                {data?.price}
+              </Text>
+            </View>
+
+            <View className="px-2 py-1 bg-teal-100 rounded-md">
+              <Text>{data?.open_now_text}</Text>
+            </View>
+          </View>
         </View>
+        <View className="mt-6 flex-col space-y-2">
+          <Text className="text-[24px] text-[#428288] font-bold">
+            {data?.name}
+          </Text>
+          <View className="flex-row space-x-2 items-center">
+            <FontAwesome name="map-marker" size={25} color="#8C9EA6" />
+            <Text className="text-[20px] text-[#8C9EA6] font-bold">
+              {data?.location_string}
+            </Text>
+          </View>
+        </View>
+        <View className="mt-4 flex-row items-center justify-between">
+          {data?.rating && (
+            <View className="flex-row items-center space-x-2">
+              <View className="w-12 h-12 rounded-2xl bg-red-100 items-center justify-center shadow-md">
+                <FontAwesome name="star" size={24} color="#D58574" />
+              </View>
+              <View>
+                <Text className="text-[#515151]">{data?.rating}</Text>
+                <Text className="text-[#515151]">Ratings</Text>
+              </View>
+            </View>
+          )}
+
+          {data?.bearing && (
+            <View className="flex-row items-center space-x-2">
+              <View className="w-12 h-12 rounded-2xl bg-red-100 items-center justify-center shadow-md">
+                <FontAwesome5 name="map-signs" size={24} color="black" />
+              </View>
+              <View>
+                <Text className="text-[#515151]">{data?.bearing}</Text>
+                <Text className="text-[#515151]">Bearing</Text>
+              </View>
+            </View>
+          )}
+
+          {data?.price_level && (
+            <View className="flex-row items-center space-x-2">
+              <View className="w-12 h-12 rounded-2xl bg-red-100 items-center justify-center shadow-md">
+                <FontAwesome5 name="dollar-sign" size={24} color="black" />
+              </View>
+              <View>
+                <Text className="text-[#515151]">{data?.price_level}</Text>
+                <Text className="text-[#515151]">Price Level</Text>
+              </View>
+            </View>
+          )}
+        </View>
+        {data?.description && (
+          <Text className="mt-4 tracking-wide text-[16px] font-semibold text-[#97A6AF]">
+            {data?.description}
+          </Text>
+        )}
+
+        {data?.cuisine && (
+          <View className="flex-row gap-2 items-center justify-start mt-4">
+            {data?.cuisine.map((n) => (
+              <TouchableOpacity
+                key={n.key}
+                className="px-2 py-1 rounded-md bg-emerald-100"
+              >
+                <Text>{n.name}</Text>
+              </TouchableOpacity>
+            ))}
+          </View>
+        )}
       </ScrollView>
     </SafeAreaView>
   );
