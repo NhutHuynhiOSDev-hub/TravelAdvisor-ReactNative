92f1e767ec89fe6206e3811c50293d197905eda1
nhuthq <>
2022-12-28 14:33:18 +0700

    add DetailsScreen + Navigation

--- components/ItemCardContainer.js
+++ components/ItemCardContainer.js
@@ -2,10 +2,13 @@ import React from "react";
 import { TouchableOpacity } from "react-native";
 import { Image, Text, View } from "react-native-animatable";
 import { FontAwesome } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 
-function ItemCardContainer({ imageSource, title, location }) {
+const ItemCardContainer = ({ imageSource, title, location }) => {
+  const navigation = useNavigation();
   return (
     <TouchableOpacity
+      onPress={() => navigation.navigate("DetailsScreen", { params: data })}
       className="rounded-md border border-gray-300 
     space-y-2 p-2 shadow-md bg-white w-[188px]"
     >
@@ -13,18 +16,25 @@ function ItemCardContainer({ imageSource, title, location }) {
         source={{ uri: imageSource }}
         className="w-full h-40 rounded-md bg-cover"
       />
-      <Text className="text-[18px] text-[#0B646B] font-bold">
-        {title?.length > 14 ? `${title?.slice(0, 14)}..` : title}
-      </Text>
 
-      <View className="flex-row space-x-1 items-center">
-        <FontAwesome name="map-marker" size={20} color="8597A2" />
-        <Text className="text-[12px] text-[#0B646B] font-bold">
-          {location?.length > 18 ? `${location?.slice(0, 18)}..` : location}
-        </Text>
-      </View>
+      {title ? (
+        <>
+          <Text className="text-[18px] text-[#0B646B] font-bold">
+            {title?.length > 14 ? `${title?.slice(0, 14)}..` : title}
+          </Text>
+
+          <View className="flex-row space-x-1 items-center">
+            <FontAwesome name="map-marker" size={20} color="8597A2" />
+            <Text className="text-[12px] text-[#0B646B] font-bold">
+              {location?.length > 18 ? `${location?.slice(0, 18)}..` : location}
+            </Text>
+          </View>
+        </>
+      ) : (
+        <></>
+      )}
     </TouchableOpacity>
   );
-}
+};
 
 export default ItemCardContainer;
--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import { useNavigation } from "@react-navigation/native";
+import React, { useLayoutEffect } from "react";
+import { Text, View } from "react-native-animatable";
 
-function DetailsScreen() {
+const DetailsScreen = ({ route }) => {
+  const navigation = useNavigation();
+  const data = route?.params?.param;
+  useLayoutEffect(() => {
+    navigation.setOptions({ headerShown: false });
+  }, []);
   return (
-    <div>DetailsScreen</div>
-  )
-}
+    <View>
+      <Text>DetailsScreen</Text>
+    </View>
+  );
+};
 
-export default DetailsScreen
+export default DetailsScreen;
